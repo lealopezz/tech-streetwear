4f2aee10c986677741dd3d9201e12f35860429d9
lealopezz <leanlopez@alumno.unlam.edu.ar>
2021-05-30 05:25:38 -0300

    nav responsive

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-let btnMenu = document.getElementById('btn-menu');
-let sideMenu = document.getElementById('sidebar-menu');
-let sideClose = document.getElementById('sidebar-close');
-btnMenu.addEventListener('click', () => {
-    sideMenu.classList.toggle('show');
+const btnMenu = document.getElementById('btnMenu');
+const menu = document.getElementById('menu');
+btnMenu.addEventListener('click',function(){
+    menu.classList.toggle('show');
 });
-sideClose.addEventListener('click',()=>{
-    sideMenu.classList.toggle('show');
-});
+
+const subMenuBtn = document.querySelectorAll('.submenu-btn');
+for(let i=0;i<subMenuBtn.length;i++){
+    subMenuBtn[i].addEventListener('click', function(){
+        if(window.innerWidth < 1024){
+            const subMenu = this.nextElementSibling;
+            const height = subMenu.scrollHeight;
+            
+            if (subMenu.classList.contains('desplegar')) {
+                subMenu.classList.remove('desplegar');
+                subMenu.removeAttribute('style');
+            } else {   
+                subMenu.classList.add('desplegar');
+                subMenu.style.height = height + "px";
+            }
+        }
+    })
+}

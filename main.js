bf984a15dc786204104b879b788cdd7e61ecc978
lealopezz <leanlopez@alumno.unlam.edu.ar>
2021-06-10 05:46:31 -0300

    submenu arrowrotation

--- main.js
+++ main.js
@@ -10,18 +10,19 @@ for(let i=0;i<subMenuBtn.length;i++){
     subMenuBtn[i].addEventListener('click', function(){
         if(window.innerWidth < 1024){
             const subMenu = this.nextElementSibling;
+            const angle = this.firstElementChild;
             const height = subMenu.scrollHeight;
             
             if (subMenu.classList.contains('desplegar')) {
                 subMenu.classList.remove('desplegar');
-                // angleDown.style.transform = 'rotate(0deg)';
-                // angleDown.style.transition = 'transform 200ms';
+                angle.classList.remove('desplegar');
+                angle.style.transform = 'rotate(0deg)';
                 subMenu.removeAttribute('style');
             } else {   
                 subMenu.classList.add('desplegar');
                 subMenu.style.height = height + "px";
-                // angleDown.style.transform = 'rotate(90deg)';
-                // angleDown.style.transition = 'transform 200ms';
+                angle.classList.add('desplegar');
+                angle.style.transform = 'rotate(-90deg)';
             }
         }
     })
